a6652324cc7678d3c8ecb8c55d705be93745ada6
pranathi02m <pranathi02m@gmail.com>
2024-10-29 21:08:56 +0530

    Initial commit

---
+++ project/bike_rental/src/main/java/com/groupproject/bike_rental/BikeRentalApplication.java
@@ -0,0 +1,13 @@
+package com.groupproject.bike_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeRentalApplication.class, args);
+	}
+
+}
---
+++ project/bike_rental/src/test/java/com/groupproject/bike_rental/BikeRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupproject.bike_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
